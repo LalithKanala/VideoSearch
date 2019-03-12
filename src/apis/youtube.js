1cd8b3b6eac640345373d1a16b41fc79291000d6
Kanala <46724893+LalithKanala@users.noreply.github.com>
2019-03-12 14:51:45 -0700

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY ='AIzaSyBIUb11DIpUM14qrkqfU2tCoFl2wdO1F-o';
+const KEY ='/* Enter your youtube API key here */';
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
